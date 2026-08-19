e1114eb6f82f0aaec90a606f20b6490d7b978fdf
bl4ck464 <blckwlf145@gmail.com>
2026-08-19 15:07:49 +0100

    Register service worker on window load

--- app.js
+++ app.js
@@ -1662,3 +1662,26 @@ document
 
 applyLanguage();
 applyTheme();
+
+if ("serviceWorker" in navigator) {
+
+  window.addEventListener("load", () => {
+
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then(registration => {
+        console.log(
+          "Service worker registered:",
+          registration.scope
+        );
+      })
+      .catch(error => {
+        console.error(
+          "Service worker registration failed:",
+          error
+        );
+      });
+
+  });
+
+}
